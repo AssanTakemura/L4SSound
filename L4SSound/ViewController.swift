1fbaee5b3c8a9ccf4bdf3118b1a9bbb5b4bd5c2f
Asuka Takemura <assan.takemura@gamil.com>
2020-09-19 13:41:30 +0900

    Initial Commit

---
+++ L4SSound/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  L4SSound
+//
+//  Created by 竹村明日香 on 2020/09/19.
+//  Copyright © 2020 Takemura assan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
