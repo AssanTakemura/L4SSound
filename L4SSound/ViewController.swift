9be045d782772c950ed57857fb65f83b27347643
Asuka Takemura <assan.takemura@gamil.com>
2020-09-20 02:21:23 +0900

    完成

--- L4SSound/ViewController.swift
+++ L4SSound/ViewController.swift
@@ -7,14 +7,66 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var drumButton: UIButton!
+    @IBOutlet var pinanoButton: UIButton!
+    @IBOutlet var guitarButton: UIButton!
+    
+    let drumSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data)
+    let pinanoSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "pianoSound")!.data)
+    let guitarSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "guitarSound")!.data)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func touchDownDrumButton(){
+        
+        drumButton.setImage(UIImage(named: "drumPlayingImage"), for: .normal)
+        drumSoundPlayer.currentTime = 0
+        drumSoundPlayer.play()
+        
+    }
+    
+    @IBAction func touchDrumButton(){
+        
+        drumButton.setImage(UIImage(named: "drumImage"), for: .normal)
+    }
+    
+    @IBAction func touchDownPianoButton(){
+        
+        pinanoButton.setImage(UIImage(named: "pianoPlayingImage"), for: .normal)
+        pinanoSoundPlayer.currentTime = 0
+        pinanoSoundPlayer.play()
+        
+    }
+    
+    @IBAction func touchUpPianoButton(){
+        
+        pinanoButton.setImage(UIImage(named: "pianoImage"), for: .normal)
+        
+    }
+    
+    @IBAction func touchDownGuitarButton(){
+        
+        guitarButton.setImage(UIImage(named: "guitarPlayingImage"), for: .normal)
+        guitarSoundPlayer.currentTime = 0
+        guitarSoundPlayer.play()
+        
+    }
+    
+    
+    @IBAction func touchUpGuitarButton(){
+        
+        guitarButton.setImage(UIImage(named: "guitarImage"), for: .normal)
+        
+    }
+    
+    
+    
 }
 
